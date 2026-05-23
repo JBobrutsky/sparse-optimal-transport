60184c6aba36318f55251d96d34e42bbd34bc445
jbobrutsky <jbobrutsky@gmail.com>
2026-05-23 12:08:11 +0300

    fix: derive __version__ from package metadata; bump to 0.2.1

--- src/sparse_ot/__init__.py
+++ src/sparse_ot/__init__.py
@@ -1,5 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .emd import emd, emd2
 from sparse_ot.feasibility import InfeasibleProblemError  # noqa: F401
 
-__version__ = "0.1.0"
+try:
+    __version__ = version("sparse-ot")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 __all__ = ["emd", "emd2", "InfeasibleProblemError"]
